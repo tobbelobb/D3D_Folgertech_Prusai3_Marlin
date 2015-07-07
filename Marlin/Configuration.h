74ae88f8fddbd4776bfee98838760eb26bc996be
Jason Baker <therippa@gmail.com>
2015-07-06 18:50:06 -0700

    Updated printer name

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -48,7 +48,7 @@
 #endif
 
 // Define this to set a custom name for your generic Mendel,
-// #define CUSTOM_MENDEL_NAME "This Mendel"
+#define CUSTOM_MENDEL_NAME "Prusa i3"
 
 // Define this to set a unique identifier for this printer, (Used by some programs to differentiate between machines)
 // You can use an online service to generate a random UUID. (eg http://www.uuidgenerator.net/version4)
